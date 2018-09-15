bec5712df740f9b5c4fc01a597a057cd01a24d01
Automated Build <c.a.hooft@gmail.com>
2018-09-15 22:01:25 +0000

    Automated Build (e2d750f5f40dd1a980a106d3850b01e541cb9628)

---
+++ sw.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const cacheVersion = '2018-09-15 22:01:24 +0000::';
+const urlsToCache = [
+  '/browserconfig.xml','/','/manifest.json','/robots.txt','/sitemap.xml','/sw.js',
+  
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheVersion + 'static').then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.indexOf(cacheVersion) !== 0;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheVersion + 'static').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function() {
+        return caches.match('/404')
+      });
+    })
+  );
+});
