1115bdff757f59bb84367aa2cba47942f7093342
Automated Build <c.a.hooft@gmail.com>
2018-09-16 21:18:00 +0000

    Automated Build (5afdc77271bafd13dea7b5986840fe7883169bcc)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const cacheVersion = '2018-09-16 21:12:06 +0000::';
+const cacheVersion = '2018-09-16 21:17:58 +0000::';
 const urlsToCache = [
   '/atom.xml','/browserconfig.xml','/','/manifest.json','/robots.txt','/sitemap.xml','/sw.js',
   
