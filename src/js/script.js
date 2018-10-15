5192be2a7ba4d8f81a2a89d908992f400fe2ed51
Casper Hooft <c.a.hooft@gmail.com>
2018-10-15 21:07:48 +0200

    cashing is now on only on the public version of my site

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
-// Register Service Worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//     // ServiceWorker registration successful
-//   }).catch(function(err) {
-//     console.log('ServiceWorker registration failed:', err);
-//   });
-// }
+---
+---
+
+{% if jekyll.environment == 'production' %}
+Register Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+    // ServiceWorker registration successful
+  }).catch(function (err) {
+    console.log('ServiceWorker registration failed:', err);
+  });
+}
+{% endif %}
 
 var vhFix = new vhFix([
   {
@@ -14,6 +19,7 @@ var vhFix = new vhFix([
   }
 ]);
 
+//images
 const backgrounds = [
   "/images/bg01.jpg",
   "/images/bg02.jpg",
@@ -23,12 +29,10 @@ const backgrounds = [
   "/images/bg06.jpg",
   "/images/bg07.jpg",
   "/images/bg08.jpg",
-  "/images/bg09.jpg",
-  "/images/bg10.jpg",
-  "/images/bg11.jpg"
-  //pictures
-]
+  "/images/bg09.jpg"
+];
+
 setInterval(function(){
-  var url=backgrounds[Math.floor(Math.random() * backgrounds.length)];
-  document.body.style.backgroundImage = 'url('+url+')';
-},10000); //wait 10 seconds
+  var url = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  document.getElementsByClassName('fullscreen')[0].style.backgroundImage = 'url("'+url+'")';
+}, 10000); //wait 10 seconds
